da62b8f3ee55318f1ac6be167468afd0a2b339d7
MeriAv <mari.avdeeva1407@gmal.com>
2020-11-09 15:10:12 +0300

    INITIAL COMMIT

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[] args) {
+        int start = 10;
+        int refill = 2300;
+        int result = start / refill;
+        int hundred = 100;
+        int calculation = refill / hundred;
+        int bonus = 0;
+        if ( calculation  >= 10) { bonus = calculation; }
+
+    }
+        System.out.println (bonus);
+}
