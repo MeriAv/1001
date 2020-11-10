b7f1c589f4def39e91eb6a50c9a87537e155e3cb
MeriAv <72850780+MeriAv@users.noreply.github.com>
2020-11-10 12:23:47 +0300

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main (String[] args) {
         int start = 10;
         int refill = 2300;
-        int result = start / refill;
         int hundred = 100;
         int calculation = refill / hundred;
         int bonus = 0;
